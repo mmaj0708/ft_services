e4bc84fa194a69440bade5e4919f49be97c5605c
mmaj0708 <djoebul@gmail.com>
2021-01-07 17:32:41 +0100

    pod nginx tested

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# start minikube
+minikube start --driver=docker
+
+
+# deploy metallb
+# add "> /dev/null" to remove info in stdout 
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
---
+++ wordpress/run_serv.sh
@@ -0,0 +1,2 @@
+service nginx restart
+tail -F /dev/null
