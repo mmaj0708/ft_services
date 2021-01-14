d94493f79406928f7845ec6258b166ff1766f2f9
mmaj0708 <djoebul@gmail.com>
2021-01-14 12:00:29 +0100

    test wp-pod

--- MariaDB/srcs/setup.sh
+++ MariaDB/srcs/setup.sh
@@ -1,4 +1,8 @@
 
-/etc/init.d/mariadb setup
-/etc/init.d/mariadb start
-mysql -u root -p < create_database.sql
+/etc/init.d/mariadb restart
+mysql --user=root << EOF
+CREATE DATABASE wordpress;
+CREATE USER 'wordpressuser'@'localhost' IDENTIFIED BY '1234';
+GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY '1234';
+FLUSH PRIVILEGES;
+EOF
