6a23e0bb5ff899ef3d6300acd57e97d3ecf9b699
mmaj0708 <djoebul@gmail.com>
2021-01-15 13:12:41 +0100

    link 2 pods

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ minikube start --driver=docker
 # add "> /dev/null" to remove info in stdout 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+# config secret for metallb
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
