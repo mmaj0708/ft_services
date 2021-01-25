79b29334e149dfb316886ed76d49b9bffb91f036
mmaj0708 <djoebul@gmail.com>
2021-01-25 13:07:40 +0100

    pma pod

--- MariaDB/srcs/run_serv.sh
+++ MariaDB/srcs/run_serv.sh
@@ -1,3 +1,15 @@
-# /etc/init.d/mariadb setup
-# /etc/init.d/mariadb restart 
+openrc
+touch /run/openrc/softlevel
+adduser -D -g 'www' www
+/etc/init.d/mariadb setup
+/etc/init.d/mariadb restart
+mysql --user=root << EOF
+CREATE DATABASE wordpress;
+CREATE USER 'admin'@'%' IDENTIFIED BY 'password';
+GRANT ALL ON wordpress.* TO 'admin'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
+CREATE USER 'admin'@'%' IDENTIFIED BY 'password';
+GRANT ALL ON *.* TO 'admin'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
+prinf "database started\n"
 tail -F /dev/null
