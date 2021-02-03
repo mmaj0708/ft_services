dc6d71cbd483196f4e4187902cb9a2bff37a3e0e
mmaj0708 <djoebul@gmail.com>
2021-02-03 13:26:32 +0100

    setga

--- setup.sh
+++ setup.sh
@@ -1,13 +1,20 @@
 # start minikube
 minikube start --driver=docker
 
+# get minikube (true) ip
+CLUSTER_IP="$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)"
 
 # deploy metallb
 # add "> /dev/null" to remove info in stdout 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+kubectl apply -f metallb-config.yaml
+
 # config secret for metallb
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
 # mettre les images en local et a jour :
 # eval $(minikube -p minikube docker-env)
+
+# installer filezilla pour ftps
+sudo apt install filezilla
