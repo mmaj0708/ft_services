7a5119433b5dfbc24bbdc65fb341382b01b23ac6
mmaj0708 <djoebul@gmail.com>
2021-02-04 16:02:53 +0100

    configure nginx

--- nginx/srcs/run_serv.sh
+++ nginx/srcs/run_serv.sh
@@ -1,3 +1,8 @@
+
+# ssh key
+openssl req -x509 -nodes -subj '/CN=localhost' -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/server.key -out /etc/ssl/certs/server.crt
+
+
 service nginx restart
-service php-fpm7 restart
+# service php-fpm7 restart
 tail -F /dev/null
