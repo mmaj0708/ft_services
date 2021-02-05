5c95a41ae6f45e35346c77489097bef43a8b1c78
mmaj0708 <djoebul@gmail.com>
2021-02-05 16:29:17 +0100

    debug + grafana + setup

--- nginx/srcs/run_serv.sh
+++ nginx/srcs/run_serv.sh
@@ -1,6 +1,4 @@
 
-# ssh key
-openssl req -x509 -nodes -subj '/CN=localhost' -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/server.key -out /etc/ssl/certs/server.crt
 
 
 service nginx restart
--- phpmyadmin/srcs/config.inc.php
+++ phpmyadmin/srcs/config.inc.php
@@ -32,6 +32,7 @@ $cfg['Servers'][$i]['auth_type'] = 'cookie';
 $cfg['Servers'][$i]['host'] = 'mysql';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['PmaAbsoluteUri'] = './';
 
 /**
  * phpMyAdmin configuration storage settings.
