cd2c644dbfc9b6e633d554350970ad597008ab78
mmaj0708 <djoebul@gmail.com>
2021-02-16 13:12:59 +0100

    begin liveness

---
+++ srcs/wordpress/srcs/livenessprob.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+printf "nginx test :"
+service nginx status
+ret_nginx=$?
+
+printf "\nphp test :"
+service php-fpm7 status
+ret_php=$?
+
+if [ $ret_nginx = 0 ] && [ $ret_php = 0 ]
+then
+    exit 0
+else
+    exit 1
+fi
